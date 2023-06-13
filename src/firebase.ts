efa4fef44d1497f81bce4e9030e029bcea6456bb
shinya u <shinyahazama7@gmail.com>
2023-06-13 10:00:52 +0900

    mod README

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAI17tUGL_9TCk9d5mFu8HhF1f-YvP9gTo",
-  authDomain: "discord-a05b0.firebaseapp.com",
-  projectId: "discord-a05b0",
-  storageBucket: "discord-a05b0.appspot.com",
-  messagingSenderId: "945239863393",
-  appId: "1:945239863393:web:8eba9cc3d270a0d4963a20"
+  apiKey: <your apiKey>,
+  authDomain: <your authDomain>,
+  projectId: <your projectId>,
+  storageBucket: <your storageBucket>,
+  messagingSenderId: <your messagingSenderId>,
+  appId: <your appId>
 };
 
 const app = initializeApp(firebaseConfig);
